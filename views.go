124a3c2234ba1da62374f86a40b047b8622f2a33
Dario Hamidi <dario@gowriteco.de>
2015-01-17 14:32:15 +0100

    Only show authenticated comments in the view.

--- views.go
+++ views.go
@@ -3,11 +3,16 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"sort"
+	"time"
 )
 
 type AllPostsComment struct {
+	Id      string
 	Author  string
 	Content string
+
+	created time.Time
 }
 
 type AllPostsPost struct {
@@ -15,10 +20,34 @@ type AllPostsPost struct {
 	Title    string
 	Content  string
 	Comments []*AllPostsComment
+
+	allComments map[string]*AllPostsComment
+}
+
+func (post *AllPostsPost) Len() int { return len(post.Comments) }
+func (post *AllPostsPost) Swap(i, j int) {
+	post.Comments[i], post.Comments[j] = post.Comments[j], post.Comments[i]
+}
+func (post *AllPostsPost) Less(i, j int) bool {
+	return post.Comments[i].created.Before(post.Comments[j].created)
+}
+
+func (post *AllPostsPost) addComment(comment *AllPostsComment) {
+	post.allComments[comment.Id] = comment
+}
+
+func (post *AllPostsPost) authenticateComment(id string) {
+	comment := post.allComments[id]
+	if comment != nil {
+		post.Comments = append(post.Comments, comment)
+		sort.Sort(post)
+	}
 }
 
 type AllPostsJSONView struct {
 	Collection []*AllPostsPost
+
+	allPosts map[string]*AllPostsPost
 }
 
 func (view *AllPostsJSONView) HandleEvent(event Event) error {
@@ -27,17 +56,28 @@ func (view *AllPostsJSONView) HandleEvent(event Event) error {
 		view.addPost(evt)
 	case *PostCommentedEvent:
 		view.addCommentToPost(evt)
+	case *PostCommentAuthenticatedEvent:
+		view.authenticateComment(evt)
 	}
 
 	return nil
 }
 
 func (view *AllPostsJSONView) addPost(evt *PostPublishedEvent) {
-	view.Collection = append(view.Collection, &AllPostsPost{
+	if view.allPosts == nil {
+		view.allPosts = map[string]*AllPostsPost{}
+	}
+
+	post := &AllPostsPost{
 		Id:      evt.PostId,
 		Title:   evt.Title,
 		Content: evt.Content,
-	})
+
+		allComments: map[string]*AllPostsComment{},
+	}
+
+	view.allPosts[evt.PostId] = post
+	view.Collection = append(view.Collection, post)
 }
 
 func (view *AllPostsJSONView) Render() []byte {
@@ -52,14 +92,18 @@ func (view *AllPostsJSONView) Render() []byte {
 
 func (view *AllPostsJSONView) addCommentToPost(evt *PostCommentedEvent) {
 	comment := &AllPostsComment{
+		Id:      evt.CommentId,
 		Author:  evt.AuthorName,
 		Content: evt.Content,
+
+		created: evt.CommentedAt,
 	}
 
-	for _, post := range view.Collection {
-		if post.Id == evt.PostId {
-			post.Comments = append(post.Comments, comment)
-			return
-		}
-	}
+	post := view.allPosts[evt.PostId]
+	post.addComment(comment)
+}
+
+func (view *AllPostsJSONView) authenticateComment(evt *PostCommentAuthenticatedEvent) {
+	post := view.allPosts[evt.PostId]
+	post.authenticateComment(evt.CommentId)
 }
